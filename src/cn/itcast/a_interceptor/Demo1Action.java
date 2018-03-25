71805519b28ef471d86c9120cd0bc8a0a4cc5e87
wang920321 <m13372782171_1@163.com>
2018-03-26 01:26:02 +0800

    配置拦截器

--- src/cn/itcast/a_interceptor/Demo1Action.java
+++ src/cn/itcast/a_interceptor/Demo1Action.java
@@ -3,6 +3,13 @@ package cn.itcast.a_interceptor;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class Demo1Action extends ActionSupport {
+    
+	
+	@Override
+	public String execute() throws Exception {
+		System.out.println("Demo1Action");
+		return SUCCESS;
+	}
 
 	public String add() throws Exception {
 		System.out.println("Demo1Action_add!");
